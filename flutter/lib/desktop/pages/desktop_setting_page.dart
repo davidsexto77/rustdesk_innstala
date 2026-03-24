7e4f9aa6e0d707386e474d9506af09dfcce2e575
davidsexto <dsextogarcia2@gmail.com>
2026-03-24 17:28:48 +0100

    Update branding from RustDesk to Innstala Support

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2369,7 +2369,7 @@ class _AboutState extends State<_About> {
       final scrollController = ScrollController();
       return SingleChildScrollView(
         controller: scrollController,
-        child: _Card(title: translate('About RustDesk'), children: [
+        child: _Card(title: translate('About Innstala Support'), children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -2388,7 +2388,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('https://innstala.com/');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -2396,7 +2396,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://innstala.com/');
                   },
                   child: Text(
                     translate('Website'),
@@ -2414,14 +2414,15 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Innstala\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
-                            translate('Slogan_tip'),
-                            style: TextStyle(
+                            'Esta es una versión personalizada y optimizada por INNSTALA basada en el proyecto de código abierto RustDesk.',
+                              style: TextStyle(
                                 fontWeight: FontWeight.w800,
-                                color: Colors.white),
+                                color: Colors.white.withOpacity(0.7), // Un pelín de transparencia para que quede elegante
+                              ),
                           )
                         ],
                       ),
